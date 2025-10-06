7c4c899940bbda5f1ca76480ac001da2910a6bed
Gokul <gokulchandan24@gmail.com>
2025-10-06 17:20:42 +0530

    a

--- versa-backend/auth_app/pipeline.py
+++ versa-backend/auth_app/pipeline.py
@@ -1,23 +1,27 @@
-from social_core.pipeline.user import create_user
 from auth_app.models import User
 
 
 def create_or_get_user(backend, details, user=None, *args, **kwargs):
-    """Custom pipeline to create or get user with credits."""
+    """
+    Custom pipeline to create or get a user with default credits for OAuth logins.
+    Ensures the returned 'user' is a Django User instance.
+    """
     if user:
-        return {'is_new': False}
+        return {'is_new': False, 'user': user}  # Always return 'user'
 
-    # Get or create user
     email = details.get('email')
-    try:
-        user = User.objects.get(email=email)
-        return {'is_new': False, 'user': user}
-    except User.DoesNotExist:
-        # Create new user with 100 credits
-        user = User.objects.create_user(
-            email=email,
-            password=None,  # No password for OAuth users
-            credits=100,
-            role='USER'
-        )
-        return {'is_new': True, 'user': user}
+    if not email:
+        return None  # Cannot create user without email
+
+    # Try to get existing user
+    user, created = User.objects.get_or_create(
+        email=email,
+        defaults={
+            'credits': 100,
+            'role': 'USER',
+            'username': email.split('@')[0],  # Required if using AbstractUser
+            'password': None  # OAuth users won't have a password
+        }
+    )
+
+    return {'is_new': created, 'user': user}
